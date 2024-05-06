e76cb2d2b843c8114e70c2c134b6267fbd994fae
safeerep <safeerep77@gmail.com>
2024-05-06 11:21:59 +0530

    fix: efficient usage of prop

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { ITodo } from "../types";
 import { BASE_URL } from "../constants";
+import toast from "react-hot-toast";
 
-const TodoItem = ({ todo }: { todo: ITodo }) => {
+const TodoItem = ({ todo, updateTodos }: { todo: ITodo, updateTodos: any }) => {
   const deleteTodoHandler = (title: string) => {
     axios
       .delete(`${BASE_URL}/remove-todo/${title}`)
-      .then((res) => console.log(res.data));
+      .then((res) => {
+        console.log(res.data)
+        toast.success("successfully deleted one item")
+        updateTodos()
+      });
   };
   return (
     <div className="bg-cyan-400 border-black border flex justify-between mt-2 text-center p-2">
